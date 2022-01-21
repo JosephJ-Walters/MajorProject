10a33fbd5059785abcce0f887b62b39449653c1e
Joseph <Joseph@JOES-DESKTOP>
2022-01-21 00:57:44 +0000

    Add project files.

---
+++ MajorProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MajorProject // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
