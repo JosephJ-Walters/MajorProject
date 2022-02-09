f3f39b79aeeb619b0ad000a2f74fdbe230aabf91
JosephJ-Walters <92579393+JosephJ-Walters@users.noreply.github.com>
2022-02-09 10:31:10 +0000

    Update Cell.cs

--- MajorProject/Cell.cs
+++ MajorProject/Cell.cs
@@ -4,6 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public struct neighboursVisited
+{
+    public neighboursVisited(int Top, int Bottom, int Left, int Right)
+    {
+        top = Top;
+        bottom = Bottom;
+        left = Left;
+        right = Right;
+    }
+
+    public int count()
+    {
+        int total = 0;
+
+        if (top != -1)
+            total += top;
+
+        if (bottom != -1)
+            total += bottom;
+
+        if (right != -1)
+            total += right;
+
+        if (left != -1)
+            total += left;
+
+        return total;
+    }
+
+    public int top = 0;
+    public int bottom = 0;
+    public int left = 0;
+    public int right = 0;
+
+
+
+
+
+};
+
+
+
 namespace MajorProject
 {
     internal class Cell
@@ -29,6 +71,52 @@ namespace MajorProject
             }
         }
 
+        public neighboursVisited returnNeighbours(Cell[,] celllist)
+        {
+            int topVisited;
+            int bottomVisited;
+            int leftVisited;
+            int rightVisited;
+            try
+            {
+                topVisited = (celllist[Coord[0], Coord[1] - 1].Visited == false) ? 1 : 0;
+            }
+            catch
+            {
+                topVisited = -1;
+            }
+
+            try
+            {
+                bottomVisited = (celllist[Coord[0], Coord[1] + 1].Visited == false) ? 1 : 0;
+            }
+            catch
+            {
+                bottomVisited = -1;
+            }
+
+            try
+            {
+                leftVisited = (celllist[Coord[0] - 1, Coord[1]].Visited == false) ? 1 : 0;
+            }
+            catch
+            {
+                leftVisited = -1;
+            }
+
+            try
+            {
+                rightVisited = (celllist[Coord[0] + 1, Coord[1]].Visited == false) ? 1 : 0;
+            }
+            catch
+            {
+                rightVisited = -1;
+            }
+            return new neighboursVisited(topVisited, bottomVisited, leftVisited, rightVisited);
+
+
+        }
+
         public void neighbours(Cell[,] celllist)
         {
             try //North cell check
@@ -40,7 +128,7 @@ namespace MajorProject
             }
             catch (Exception ex)
             {
-                Console.WriteLine("No cell north");
+                //Console.WriteLine("No cell north");
             }
 
             try //South cell check
@@ -52,7 +140,7 @@ namespace MajorProject
             }
             catch (Exception ex)
             {
-                Console.WriteLine("No cell south");
+                //Console.WriteLine("No cell south");
             }
 
             try //West cell check
@@ -64,7 +152,7 @@ namespace MajorProject
             }
             catch (Exception ex)
             {
-                Console.WriteLine("No cell west");
+                //Console.WriteLine("No cell west");
             }
 
             try //east cell check
@@ -76,7 +164,7 @@ namespace MajorProject
             }
             catch (Exception ex)
             {
-                Console.WriteLine("No cell east");
+                //Console.WriteLine("No cell east");
             }
         }
     }
